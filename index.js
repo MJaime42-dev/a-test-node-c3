fee450e1c71842f8a936ba6ce087666bad0b370f
MJaime42 <113955687+MJaime42@users.noreply.github.com>
2023-11-03 13:36:26 -0500

    update

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.get('/test', function(request, response) {
 	response.type('text/plain')
 	response.send('Node.js and Express running on port='+port)
 })
+// Load your JSON data
+const favoritePlaces = require('./FavoritePlaces.json');
+
+// Create a route that serves the JSON data
+app.get('/api/favorite-places', (req, res) => {
+  res.json(favoritePlaces);
+});
 
 app.listen(port, function() {
 	console.log("Server is running at http://localhost:3000/")
